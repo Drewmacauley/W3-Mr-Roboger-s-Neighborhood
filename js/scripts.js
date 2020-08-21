e34652207cc9c6710e83930ef615cb8fecc912f8
Drewmacauley <macauleyandy03@gmail.com>
2020-08-21 12:14:16 -0700

    add script/more trial and error

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
-$(document).ready(function)() {
+$(document).ready(function() {
   $("form#numberForm").submit(function(event) {
     event.preventDefault();
     const formInt = parseInt($("input#numberInput").val());
 
     let newArr = [formInt];
-    for (let i=o; i < formInt; i ++ 1) {
+    for (let i=o; i < formInt; i =+ 1) {
       let newInt = (newArr[i]-1);
       if (newInt === 0) {
         break;
-      } 
+      };
+      newArr.push(newInt); 
     }
+    let facNum = newArr.reduce((a,b) => a * b);
+    $("result").text(facNum);
+
   });
 });
 
