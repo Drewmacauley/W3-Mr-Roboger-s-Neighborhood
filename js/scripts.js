36e7aeb8a96328f30b35395269204e0ccc37154a
Drewmacauley <macauleyandy03@gmail.com>
2020-08-21 08:43:37 -0700

    add README, plan of attack

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//create a code to input 0-?
+
+
+//Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+
+
+
+//Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+
+
+
+//Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"
