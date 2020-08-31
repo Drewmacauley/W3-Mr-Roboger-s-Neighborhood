e7e59ebad15a3489a7132e52d7d558622926a7da
Drewmacauley <macauleyandy03@gmail.com>
2020-08-30 21:50:08 -0700

    save changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,76 @@
-function threeDig(number) {      //Should return a value not JQ
-  if (number === 1) {
-   $("#numberInput").text("Beep");
-  }
-  else if (number === 2) {
-   $("#numberInput").text("Boop");
-  }
- else if (number === 3) {
-    $("#numberInput").text("Wont you be my neighbor"); 
-  });
+// import java.util.*; 
+// class FizzBuzz 
+// { 
+//     public static void main(String args[]) 
+//     {  
+//         int n = 100; 
+//         for (int i=1; i<=n; i++)                                  
+//         { 
+//             if (i%15==0)                                                  
+//                 System.out.print("FizzBuzz"+" ");  
+//             else if (i%5==0)      
+//                 System.out.print("Buzz"+" ");  
+//             else if (i%3==0)      
+//                 System.out.print("Fizz"+" ");  
+//             else // print the numbers 
+//                 System.out.print(i+" ");                          
+//         } 
+//     } 
+// } 
+
+
+
+
+
+
+
+
+const threeDig = [1,2,3];
+let numName = [];
+threeDig.forEach(function(1){
+  numName.push(1 === Beep);
 });
 
-    let newArr = [];
-    for (let i=0; i < formInt; i += 1) {
-      let newInt = (newArr[i]-1);        //peace out newInt add 3dig
-     if (newInt === 0) {
-        break;
-      };
-      newArr.push(newInt);      //output newArr with JQ.....not sure what that means
-    }
-   let facNum = newArr.reduce((a,b) => a * b);
-    $("result").text(facNum);
 
 
 
 
 
+// const array = [0,1,2,3,4,5];
+// let doubledArray = [];
+// array.forEach(function(element) {
+//   doubledArray.push(element * 2);
+// });
+// doubledArray;
+
+
+
+// let newArr = [];
+//   for (let i=0; i < formInt; i += 1) {
+//     let newInt = (newArr[i]-1);        //peace out newInt add 3dig.......Also turn 3dig into a string......not sure how yet
+//     if (newInt === 0) {                 //include the add 1 method......
+//       break;
+//     };
+//     newArr.push(newInt);      //output newArr with JQ.....not sure what that means
+//   }
+//   let facNum = newArr.reduce((a,b) => a * b);
+//   $("result").text(facNum);
+
+
+
+
+    // function threeDig(number) {      //Should return a value not JQ
+      //   if (number === 1) {
+      //    $("#numberInput").text("Beep");
+      //   }
+      //   else if (number === 2) {
+      //    $("#numberInput").text("Boop");
+      //   }
+      //  else if (number === 3) {
+      //     $("#numberInput").text("Wont you be my neighbor"); 
+      //   });
+      // });
+      
 
 //create a code to input 0-?
 
